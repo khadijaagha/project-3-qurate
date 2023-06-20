6cbde641574b7a494317f6e2cb617219a16b1e44
treeve <tcv.reeve@gmail.com>
2023-06-20 10:57:54 +0100

    authentication functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date = models.DateField('Creation Date')
