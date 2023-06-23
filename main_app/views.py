d7a926b2a1d25d0f1084e83b06183c7c43ad692c
khadijaagha <dija28@gmail.com>
2023-06-23 12:23:51 +0100

    save6

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 # from django.contrib.auth.forms import UserCreationForm
-from main_app.forms import UserCreationForm
+from forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile, Post, Comment
